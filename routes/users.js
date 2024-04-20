00a7c2f915e0d53b0d9be77e2f0c9e3450089798
Haris khan <89725754+Hariskhan2@users.noreply.github.com>
2024-04-20 12:33:33 +0500

    Update the UI of the login, register and dashboard screens

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const express =require( "express");
 const router= express.Router();
 
 router.get('/login', (req, res)=>{
-    res.send("Login Page");
+    res.render("login");
 })
 
 router.get('/register', (req, res)=>{
-    res.send("Register Page");
+    res.render("register");
 })
 module.exports = router;
