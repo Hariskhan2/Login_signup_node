415dce171763bf0799622635253a3d2c9134eefb
Haris khan <89725754+Hariskhan2@users.noreply.github.com>
2024-04-21 18:29:31 +0500

    Add the login and logout functionality with the bycryption of passwod

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-const express =require( "express");
-const expressLayouts= require("express-ejs-layouts")
-const app= express();
-const mongoose= require("mongoose");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const  passport = require("passport");
+
+require('./config/passport')(passport)
 const PORT = process.env.PORT || 5000;
-
-
 
 //DB config
 
-const db= require("./config/keys").MongoURI;
+const db = require("./config/keys").MongoURI;
 
 //vonect to mongo
 
-mongoose.connect(db, {useNewUrlParser: true})
-.then(()=> console.log("DB Connected"))
-.catch(err => console.log(err))
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+// Body[arser]
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 
-app.use(express.urlencoded({extended: false}))
+app.use(flash());
 
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+
+  next();
+});
 //ejs
 
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //router routes
 
-app.use("/", require('./routes/index'))
-app.use("/users", require('./routes/users'))
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 // body parser
-
-
-
 
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
